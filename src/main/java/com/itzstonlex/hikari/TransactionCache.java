16cb0a224d3b23b983cea6182c4e08ae4aff0fd4
itzstonlex <seltixfromhell@bk.ru>
2022-09-07 00:48:31 +0400

    optimize TransactionCache size

--- src/main/java/com/itzstonlex/hikari/TransactionCache.java
+++ src/main/java/com/itzstonlex/hikari/TransactionCache.java
@@ -5,14 +5,23 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class TransactionCache {
 
+    private static final int MAX_CACHE_SIZE = 1024 * 1024;
+
     private final Map<Integer, Query> cache = new ConcurrentHashMap<>();
 
-    public boolean contains(Query query) {
-        return cache.containsKey(query.hashCode());
+    private void checkSize() {
+        if (cache.size() >= MAX_CACHE_SIZE) {
+            cache.clear();
+        }
     }
 
     public void push(Query query) {
+        checkSize();
         cache.put(query.hashCode(), query);
+    }
+
+    public void pushAll(TransactionCache parent) {
+        parent.cache.values().forEach(this::push);
     }
 
     public Query peek(int hashCode) {
